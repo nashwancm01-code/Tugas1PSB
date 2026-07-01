75ee98d596139ad10adf2bc666d3afe6ee9b34af
nashwancm01-code <nashwancm01@gmail.com>
2026-07-01 13:50:26 +0700

    Remove filter calculation description from app.py
    Removed the description of the filter calculation method.

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Butterworth Filter Orde 2", layout="wide")
 
 st.title("Desain Filter Digital Butterworth Orde 2")
-st.write("Dihitung murni menggunakan *Bilinear Transformation* tanpa library SciPy/Numpy.")
 
 # --- INPUT UTAMA ---
 Ts = st.number_input("Tsampling (s):", value=0.01, step=0.001, format="%.4f")
